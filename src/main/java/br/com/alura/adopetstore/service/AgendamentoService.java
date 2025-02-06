27d5104852f0de129ebca9e0623a1439cda32cb8
jeffersonrolino <jefferson.rolino@gmail.com>
2025-02-05 22:05:17 -0300

    Capítulo 04 - Aula 07: Boas práticas ao utilizar CompletableFuture

--- src/main/java/br/com/alura/adopetstore/service/AgendamentoService.java
+++ src/main/java/br/com/alura/adopetstore/service/AgendamentoService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 @Service
@@ -19,6 +20,8 @@ public class AgendamentoService {
     public void envioDeEmailsAgendados(){
         var estoqueZerado = relatorioService.infoEstoque();
         var faturamentoObtido = relatorioService.faturamentoObtido();
+
+        CompletableFuture.allOf(estoqueZerado, faturamentoObtido).join();
 
         try {
             enviador.enviar(estoqueZerado.get(), faturamentoObtido.get());
